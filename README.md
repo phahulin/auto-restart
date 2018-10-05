55fe674ab742a21077a1f22b570a9fa18c8edefb
phahulin <p.hahulin@hintsolutions.ru>
2018-10-06 02:15:44 +0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# auto-restart
+# Auto Restart
 Simple bash script to automatically restart services if they fail too often
+
+## How-to
+1. create the following files
+```bash
+touch ar.conf # optional
+touch test-command
+touch restart-command
+touch notify-command
+```
+2. when script is started, content of `test-command` file is executed. If it returns non-zero exit code, then under certain conditions, content of `restart-command` file will be executed.
+3. restart only happens if 
+    a. `test-command` failed at least `AR_FAILS_BEFORE_RESTART` times
+    b. failures happen sufficiently often (of the latest `AR_FAILS_BEFORE_RESTART` failures, the earliest one and the last one are separated by less than `AR_FAILS_MAX_SEP` seconds)
+    c. latest restart didn't happen too recently (at least `AR_RESTARTS_MIN_SEP` seconds should pass between restarts)
+4. when a restart happens, `notify-command` is also executed
+5. timestamps of failures and restarts are written to `AR_FAILS_LOG` and `AR_RESTARTS_LOG`, these files should be preserved for correct work of the script
+6. environment variables can be read from `ar.conf` file if it exists. Full list of env variables and their default values is:
+```bash
+export AR_DEBUG=0                               # additional logging (0/1)
+export AR_SERVICE=$(hostname)                   # name of the service
+export AR_LOG=ar.log                            # main log file
+export AR_FAILS_LOG=ar-fails.log                # log file to store failures
+export AR_RESTARTS_LOG=ar-restarts.log          # log file to store restarts
+export AR_EXEC_LOG=ar-exec-out.log              # stdout for *-command. Use /dev/null to ignore output
+export AR_EXEC_ERR=ar-exec-err.log              # stderr for *-command. Use /dev/null to ignore output
+export AR_FAILS_BEFORE_RESTART=3                # min N of failures to trigger restart
+export AR_FAILS_MAX_SEP=60                      # max separation of failures in seconds so that they count as consequtive
+export AR_RESTARTS_MIN_SEP=600                  # min number of seconds between two restarts
+export AR_TEST_COMMAND_FILE=test-command        # name of the file with test commands
+export AR_RESTART_COMMAND_FILE=restart-command  # name of the file with restart command
+export AR_NOTIFY_COMMAND_FILE=notify-command    # name of the file with notification command
+export AR_RESTART=1                             # switch to quickly enable/disable restarts
+export AR_NOTIFY=1                              # switch to quickly enable/disable notifications
+```
+7. exit code of this script is
+    * `0` if test passed
+    * `1` if test failed, but restart wasn't triggered
+    * `2` if restart was or should have been triggered (depending on values of `AR_RESTART`)
+    * `3` if initial validation failed and one of `*-command` files doesn't exist or is empty 
---
+++ ar.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/env bash
+
+#### required local files
+## * test-command
+##     - contains script to be executed to test if service works
+## * restart-command
+##     - contains script to be executed if tests fail
+## * notify-command
+##     - contains script to be executed on restart
+#
+#### env variables can be redefined in ar.conf file
+
+set -e
+set -u
+
+# read environment from ar.conf file if it exists
+AR_CONF="ar.conf"
+set -a
+if [ -f $AR_CONF ]; then
+	echo "Reading environment from $AR_CONF"
+	source "$AR_CONF"
+fi
+set +a
+
+# apply defaults
+export AR_DEBUG=${AR_DEBUG:-0}
+export AR_SERVICE=${AR_SERVICE:-$(hostname)}
+## log files for ar
+export AR_LOG=${AR_LOG:-ar.log}
+export AR_FAILS_LOG=${AR_FAILS_LOG:-ar-fails.log}
+export AR_RESTARTS_LOG=${AR_RESTARTS_LOG:-ar-restarts.log}
+## log files when executing external tasks
+export AR_EXEC_LOG=${AR_EXEC_LOG:-ar-exec-out.log}
+export AR_EXEC_ERR=${AR_EXEC_ERR:-ar-exec-err.log}
+## consequtive fails to cause a restart
+export AR_FAILS_BEFORE_RESTART=${AR_FAILS_BEFORE_RESTART:-3}
+## max separation in seconds between fails to still count them as consequtive
+export AR_FAILS_MAX_SEP=${AR_FAILS_MAX_SEP:-60}
+## min separation in seconds between consequtive restarts
+export AR_RESTARTS_MIN_SEP=${AR_RESTARTS_MIN_SEP:-600}
+## action functions
+export AR_TEST_COMMAND_FILE=${AR_TEST_COMMAND_FILE:-test-command}
+export AR_RESTART_COMMAND_FILE=${AR_RESTART_COMMAND_FILE:-restart-command}
+export AR_NOTIFY_COMMAND_FILE=${AR_NOTIFY_COMMAND_FILE:-notify-command}
+## simple switches
+export AR_RESTART=${AR_RESTART:-1}
+export AR_NOTIFY=${AR_NOTIFY:-1}
+
+log() {
+	echo "$(date -u +%Y-%m-%d"T"%H:%M:%S)" "$AR_SERVICE" "$@" >> $AR_LOG
+}
+
+if [ "$AR_DEBUG" -ne "0" ]; then
+	dbg() {
+		log "$@"
+	}
+else
+	dbg() {
+		:
+	}
+fi
+
+enabled_str() {
+	if [ "$1" -ne "0" ]; then
+		echo "enabled"
+	else
+		echo "disabled"
+	fi
+}
+
+check_file() {
+	dbg "Checking file for $2: $1"
+	if [ ! -f "$1" ]; then
+		log "File for $2 not found: $1"
+		log "Exiting"
+		exit 3
+	fi
+
+	if [ ! -s "$1" ]; then
+		log "File for $2 is empty: $1"
+		log "Exiting"
+		exit 3
+	fi
+}
+
+execute_file() {
+	dbg "Executing $2 from: $1"
+	bash "$1" >> $AR_EXEC_LOG 2>>$AR_EXEC_ERR
+	echo $?
+}
+
+notify() {
+	if [ "$AR_NOTIFY" -ne "0" ]; then
+		dbg "Send notification"
+		execute_file "$AR_NOTIFY_COMMAND_FILE" "notify command" > /dev/null
+	else
+		log "Do not send notification because AR_NOTIFY is $AR_NOTIFY"
+	fi
+}
+
+do_restart() {
+	echo "$AR_SERVICE" "$(date -u +%s)" >>$AR_RESTARTS_LOG
+	notify
+	if [ "$AR_RESTART" -ne "0" ]; then
+		execute_file "$AR_RESTART_COMMAND_FILE" "restart command" > /dev/null
+	else
+		log "Do not restart because AR_RESTART is $AR_RESTART"
+	fi
+}
+
+failed() {
+	echo "$AR_SERVICE" "$(date -u +%s)" >>$AR_FAILS_LOG
+	grep "^$AR_SERVICE " "$AR_FAILS_LOG" | tail -n "$AR_FAILS_BEFORE_RESTART" > .ar-last-fails.tmp
+	if [ "$(wc -l < .ar-last-fails.tmp)" -lt "$AR_FAILS_BEFORE_RESTART" ]; then
+		dbg "Too few fails to trigger a restart (< $AR_FAILS_BEFORE_RESTART)"
+		echo 1
+	else
+		dbg "Check if latest fails are separated by more than $AR_FAILS_MAX_SEP seconds"
+		local fail_sep=$(awk '{ if (NR==1) f=$2 } END { print $2-f }' .ar-last-fails.tmp)
+		if [ "$fail_sep" -lt "$AR_FAILS_MAX_SEP" ]; then
+			dbg "Check if the last restart was too recently"
+			touch "$AR_RESTARTS_LOG"
+			local last_restart="$(grep "^$AR_SERVICE " "$AR_RESTARTS_LOG" | tail -n 1 | awk '{ print $2 }')"
+			if [ -z "$last_restart" ]; then
+				last_restart="0"
+			fi
+			local now_ts="$(date -u +%s)"
+			local restart_sep="$(( now_ts - last_restart ))"
+			if [ "$restart_sep" -ge "$AR_RESTARTS_MIN_SEP" ]; then
+				log "Do restart"
+				do_restart
+				echo 2
+			else
+				log "Last restart was too recently, do not restart"
+				echo 1
+			fi
+		else
+			log "Latest fails are too separated (> $AR_FAILS_MAX_SEP seconds), do not restart"
+			echo 1
+		fi
+	fi
+}
+
+main() {
+	log "Starting, restarts: $(enabled_str "$AR_RESTART"), notifications: $(enabled_str "$AR_NOTIFY")"
+
+	check_file "$AR_TEST_COMMAND_FILE" "test_command"
+	if [ "$AR_RESTART" -ne "0" ]; then
+		check_file "$AR_RESTART_COMMAND_FILE" "restart command"
+	fi
+	if [ "$AR_NOTIFY" -ne "0" ]; then
+		check_file "$AR_NOTIFY_COMMAND_FILE" "notify command"
+	fi
+
+	local result="$(execute_file $AR_TEST_COMMAND_FILE "test command")"
+	dbg "Test command completed, result: $result"
+	if [ "$result" -ne "0" ]; then
+		log "Test command failed"
+		failed
+	else
+		log "Test command passed"
+	fi
+}
+
+main
